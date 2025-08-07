6da582f3bc4427e98d3da3bcca816a1a1bab5eb4
epena30 <epena379@hotmail.com>
2025-08-06 17:27:43 -0700

    Create cs file for the Mindfulness program.cs

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,9 @@
+using System;
+
+class ListingActivity
+{
+    static void Main(string[] args)
+    {
+    
+    }
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,9 @@
+using System;
+
+class ReflectingActivity
+{
+    static void Main(string[] args)
+    {
+    
+    }
+}
